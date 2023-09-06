46de6c22a31e6e9faca2287fd45657e7b35176a1
Tendani2000 <127980784+Tendani2000@users.noreply.github.com>
2023-09-06 12:07:28 +0200

    Encapsulation

--- DWA_08.3 Challenge 1.js/bookPreview.js
+++ DWA_08.3 Challenge 1.js/bookPreview.js
@@ -1,19 +1,26 @@
-function createBookPreviewElement({ author, id, image, title }) {
-    const element = document.createElement('button');
-    element.classList = 'preview';
-    element.setAttribute('data-preview', id);
+export { createBookPreviews };
 
-    element.innerHTML = `
-        <img
-            class="preview__image"
-            src="${image}"
-        />
-        
-        <div class="preview__info">
-            <h3 class="preview__title">${title}</h3>
-            <div class="preview__author">${authors[author]}</div>
-        </div>
-    `;
+function createBookPreviews(matches, BOOKS_PER_PAGE, authors) {
+    const starting = document.createDocumentFragment();
 
-    return element;
+    for (const { author, id, image, title } of matches.slice(0, BOOKS_PER_PAGE)) {
+        const element = document.createElement('button');
+        element.classList = 'preview';
+        element.setAttribute('data-preview', id);
+
+        element.innerHTML = `
+            <img
+                class="preview__image"
+                src="${image}"
+            />
+            
+            <div class="preview__info">
+                <h3 class="preview__title">${title}</h3>
+                <div class="preview__author">${authors[author]}</div>
+            </div>
+        `;
+        starting.appendChild(element);
+    }
+
+    document.querySelector('[data-list-items]').appendChild(starting);
 }
